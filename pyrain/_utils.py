d893ea1fd3392f9efc621b77d72a948dbd2e48f2
Darshan Baral <darshanbaral@gmail.com>
2022-04-28 17:43:46 -0700

    added function for obtaining water year

--- pyrain/_utils.py
+++ pyrain/_utils.py
@@ -1,19 +1,28 @@
 import pandas
 from typing import Union
-from datetime import datetime
 
 
-def _to_timestamp(freq: Union[str, pandas.Timedelta],
-                  origin: datetime = datetime(2017, 1, 1)):
+def _to_timestamp(freq: Union[str, pandas.Timedelta]) -> pandas.Timestamp:
     """
 
-    :param freq:
-    :param origin:
-    :return:
+    :param freq: pandas offset string
     """
+
+    origin = pandas.to_datetime("2017-01-01")
     if isinstance(freq, str):
         return origin + pandas.tseries.frequencies.to_offset(freq)
     elif isinstance(freq, pandas.Timedelta):
         return origin + freq
     else:
         raise ValueError("input should either be valid offset alias or Timedelta")
+
+
+def _get_water_year(idx: pandas.DatetimeIndex, year_start: int) -> pandas.Index:
+    """
+
+    :param idx: datetime index
+    :param year_start: month when the water year starts
+    :return:
+    """
+    water_year = idx.map(lambda x: x.year if (x.month < year_start) | (year_start == 1) else x.year + 1)
+    return water_year
