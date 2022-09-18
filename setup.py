e5507c9ad4c1a34cf7b4f14a206a861773b73275
Darshan Baral <darshanbaral@gmail.com>
2022-09-18 11:35:36 -0700

    updated `Mako` version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyrain',
-    version='1.0.0',
+    version='1.0.1',
     packages=['pyrain'],
     url='https://github.com/darshanbaral/pyrain',
     license='MIT',
